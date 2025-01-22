cc4d573c11d7f65388a001062d55c81cd6d2794f
Jonathan Borges <mborges.jonathan@gmail.com>
2025-01-22 15:47:44 -0300

    add jobs.json de forma estática em todo o site para hospedagem em frontend somente

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/Hero";
 import HomeCards from "../components/HomeCards";
 import JobListings from "../components/JobListings";
 import ViewAllJobs from "../components/ViewAllJobs";
+import { toast } from "react-toastify";
 
 const HomePage = () => {
+  useEffect(() => {
+    toast.warning(
+      "Aviso: Este site é estático. Para acessar a versão dinâmica com CRUD funcional, visite: github.com/seu-repositorio"
+    );
+  }, []);
+
   return (
     <>
       <Hero />
